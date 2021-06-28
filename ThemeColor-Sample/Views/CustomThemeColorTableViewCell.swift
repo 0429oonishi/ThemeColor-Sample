d7d87867f6567c6e7481ce27c49bdc146453608c
0429oonishi <oonishireon110@gmail.com>
2021-06-28 23:28:58 +0900

    カスタムセル実装

--- ThemeColor-Sample/Views/CustomThemeColorTableViewCell.swift
+++ ThemeColor-Sample/Views/CustomThemeColorTableViewCell.swift
@@ -7,17 +7,30 @@
 
 import UIKit
 
-class CustomThemeColorTableViewCell: UITableViewCell {
+final class CustomThemeColorTableViewCell: UITableViewCell {
 
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var view: UIView!
+    
+    static var identifier: String { String(describing: self) }
+    static var nib: UINib { UINib(nibName: String(describing: self), bundle: nil) }
+    var onTapEvent: (() -> Void)?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(didTapped))
+        self.view.addGestureRecognizer(tapGR)
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    @objc private func didTapped() {
+        onTapEvent?()
+    }
+    
+    func configure(row: Row, onTapEvent: @escaping () -> Void) {
+        titleLabel.text = row.title
+        self.onTapEvent = onTapEvent 
     }
     
 }
