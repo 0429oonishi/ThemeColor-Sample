50990866fa5587086ed49dc19e01fb65f1033654
0429oonishi <oonishireon110@gmail.com>
2021-06-28 23:28:29 +0900

    カスタムセクションとカスタムテーブル作成

---
+++ ThemeColor-Sample/Views/CustomThemeColorTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomThemeColorTableViewCell.swift
+//  ThemeColor-Sample
+//
+//  Created by 大西玲音 on 2021/06/28.
+//
+
+import UIKit
+
+class CustomThemeColorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ThemeColor-Sample/Views/SectionHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  SectionHeaderView.swift
+//  ThemeColor-Sample
+//
+//  Created by 大西玲音 on 2021/06/28.
+//
+
+import UIKit
+
+class SectionHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
