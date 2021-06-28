aabbe872cd32a9f745df77eebd946605845baf36
0429oonishi <oonishireon110@gmail.com>
2021-06-28 22:28:24 +0900

    VCファイル作成

---
+++ ThemeColor-Sample/Controllers/SettingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingViewController.swift
+//  ThemeColor-Sample
+//
+//  Created by 大西玲音 on 2021/06/28.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
