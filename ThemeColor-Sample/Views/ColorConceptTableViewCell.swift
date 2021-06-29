a2f154a8c021c9122dc3f08b640ddcc43e25ab25
0429oonishi <oonishireon110@gmail.com>
2021-06-29 23:27:42 +0900

    cell作成

---
+++ ThemeColor-Sample/Views/ColorConceptTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ColorConceptTableViewCell.swift
+//  ThemeColor-Sample
+//
+//  Created by 大西玲音 on 2021/06/29.
+//
+
+import UIKit
+
+class ColorConceptTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
