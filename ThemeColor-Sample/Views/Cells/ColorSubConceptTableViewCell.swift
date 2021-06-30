a9b9020c159e8307d84ca5dc8baa76e11ce3fa79
0429oonishi <oonishireon110@gmail.com>
2021-07-01 02:48:22 +0900

    cell作成と改装変更

---
+++ ThemeColor-Sample/Views/Cells/ColorSubConceptTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ColorSubConceptTableViewCell.swift
+//  ThemeColor-Sample
+//
+//  Created by 大西玲音 on 2021/07/01.
+//
+
+import UIKit
+
+class ColorSubConceptTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
