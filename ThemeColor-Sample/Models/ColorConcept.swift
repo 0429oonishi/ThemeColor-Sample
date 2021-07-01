2cff1437be57ba542998d63eeefe4a61e5c31a68
0429oonishi <oonishireon110@gmail.com>
2021-07-02 04:07:10 +0900

    NaturalConcept実装

--- ThemeColor-Sample/Models/ColorConcept.swift
+++ ThemeColor-Sample/Models/ColorConcept.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-enum ColorConcept: Int, CaseIterable {
+enum ColorConcept: CaseIterable {
     case natural
     case pop
     case elegant
@@ -68,88 +68,3 @@ enum ColorConcept: Int, CaseIterable {
     }
 }
 
-enum NaturalConcept: CaseIterable {
-    case relax
-    case feminine
-    case organic
-    case craft
-    case living
-    case botanical
-    var title: String {
-        switch self {
-            case .relax: return "リラックス"
-            case .feminine: return "フェミニン"
-            case .organic: return "オーガニック"
-            case .craft: return "クラフト"
-            case .living: return "リビング"
-            case .botanical: return "ボタニカル"
-        }
-    }
-    var colors: [UIColor] {
-        switch self {
-            case .relax: return [.Natural.Relax.eggIvory,
-                                 .Natural.Relax.softBuff,
-                                 .Natural.Relax.mistMossGreen]
-            case .feminine: return [.Natural.Feminine.grandmaPink,
-                                    .Natural.Feminine.blueVanilla,
-                                    .Natural.Feminine.grandmaPurple]
-            case .organic: return [.Natural.Organic.pattyGray,
-                                   .Natural.Organic.classicPink,
-                                   .Natural.Organic.smoothGreen]
-            case .craft: return [.Natural.Craft.twilightGray,
-                                 .Natural.Craft.sharpGray,
-                                 .Natural.Craft.japaneseKai]
-            case .living: return [.Natural.Living.creamBlue,
-                                  .Natural.Living.northOcean,
-                                  .Natural.Living.shineMuscat]
-            case .botanical: return [.Natural.Botanical.clearWater,
-                                     .Natural.Botanical.fineweed,
-                                     .Natural.Botanical.babyLeaf]
-        }
-    }
-}
-
-enum PopConcept {
-    
-}
-
-private extension UIColor {
-    
-    struct Natural {
-        struct Relax {
-            static let eggIvory = UIColor(hex: "f3eed5")
-            static let softBuff = UIColor(hex: "e4af9b")
-            static let mistMossGreen = UIColor(hex: "d4dfbb") 
-        }
-        struct Feminine {
-            static let grandmaPink = UIColor(hex: "e6c5cf")
-            static let blueVanilla = UIColor(hex: "bdd8dd")
-            static let grandmaPurple = UIColor(hex: "af9dc0")
-        }
-        struct Organic {
-            static let pattyGray = UIColor(hex: "d9d8ce")
-            static let classicPink = UIColor(hex: "e0b6a9")
-            static let smoothGreen = UIColor(hex: "d3e9d0")
-        }
-        struct Craft {
-            static let twilightGray = UIColor(hex: "dcd5c8")
-            static let sharpGray = UIColor(hex: "a2a2ad")
-            static let japaneseKai = UIColor(hex: "1f1e63")
-        }
-        struct Living {
-            static let creamBlue = UIColor(hex: "e0f1f1")
-            static let northOcean = UIColor(hex: "5188b1")
-            static let shineMuscat = UIColor(hex: "aed265")
-        }
-        struct Botanical {
-            static let clearWater = UIColor(hex: "d9eaec")
-            static let fineweed = UIColor(hex: "74a64c")
-            static let babyLeaf = UIColor(hex: "bcd537")
-        }
-    }
-    
-    struct Pop {
-        
-    }
-    
-}
