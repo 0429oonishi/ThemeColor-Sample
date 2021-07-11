b8277f3c13aa8638d796d212c8066c51a5ff54b8
0429oonishi <oonishireon110@gmail.com>
2021-07-11 20:08:08 +0900

    リネームと階層変更

--- ThemeColor-Sample/Views/Cells/AccordionTableViewCell.swift
+++ ThemeColor-Sample/Views/Cells/AccordionTableViewCell.swift
@@ -1,5 +1,5 @@
 //
-//  CustomThemeColorTableViewCell.swift
+//  AccordionTableViewCell.swift
 //  ThemeColor-Sample
 //
 //  Created by 大西玲音 on 2021/06/28.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class CustomThemeColorTableViewCell: UITableViewCell {
+final class AccordionTableViewCell: UITableViewCell {
     
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var view: UIView!
