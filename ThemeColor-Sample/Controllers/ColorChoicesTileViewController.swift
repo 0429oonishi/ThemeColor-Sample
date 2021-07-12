93ebe0d547320d122ca7260bf3279950fd349f91
0429oonishi <oonishireon110@gmail.com>
2021-07-13 04:57:16 +0900

    alpha値も設定できるように変更

--- ThemeColor-Sample/Controllers/ColorChoicesTileViewController.swift
+++ ThemeColor-Sample/Controllers/ColorChoicesTileViewController.swift
@@ -7,6 +7,50 @@
 
 import UIKit
 
+enum TileColorType: Int, CaseIterable {
+    case licorice
+    case maraschino
+    case tangerine
+    case lemon
+    case spring
+    case seaFoam
+    case turquoise
+    case aqua
+    case blueberry
+    case grape
+    case magenta
+    case strawberry
+    
+    func loadColor(alpha: CGFloat) -> UIColor {
+        switch self {
+            case .licorice:
+                return  UIColor.rgba(red: 0, green: 0, blue: 0, alpha: alpha)
+            case .maraschino:
+                return UIColor.rgba(red: 255, green: 38, blue: 0, alpha: alpha)
+            case .tangerine:
+                return UIColor.rgba(red: 255, green: 147, blue: 0, alpha: alpha)
+            case .lemon:
+                return UIColor.rgba(red: 255, green: 251, blue: 0, alpha: alpha)
+            case .spring:
+                return UIColor.rgba(red: 0, green: 249, blue: 0, alpha: alpha)
+            case .seaFoam:
+                return UIColor.rgba(red: 0, green: 250, blue: 146, alpha: alpha)
+            case .turquoise:
+                return UIColor.rgba(red: 0, green: 253, blue: 255, alpha: alpha)
+            case .aqua:
+                return UIColor.rgba(red: 0, green: 150, blue: 255, alpha: alpha)
+            case .blueberry:
+                return UIColor.rgba(red: 4, green: 51, blue: 255, alpha: alpha)
+            case .grape:
+                return UIColor.rgba(red: 148, green: 55, blue: 255, alpha: alpha)
+            case .magenta:
+                return UIColor.rgba(red: 255, green: 64, blue: 255, alpha: alpha)
+            case .strawberry:
+                return UIColor.rgba(red: 255, green: 47, blue: 146, alpha: alpha)
+        }
+    }
+}
+
 protocol ThemeColorTileViewDelegate: AnyObject {
     func tileViewDidTapped(selectedView: UIView)
 }
@@ -59,17 +103,19 @@ final class ColorChoicesTileViewController: UIViewController {
     }
     
     private func setupTileViews() {
-        var alpha: CGFloat = 1
-        tileStackViews.forEach { stackView in
-            stackView.arrangedSubviews
-                .map { $0 as! ThemeColorTileView }
-                .forEach {
-                    $0.delegate = self
-                    $0.alpha = alpha
-                    alpha -= 0.1
-                }
-            alpha = 1
-        }
+        tileStackViews
+            .enumerated()
+            .forEach { verticalCount, stackView in
+                stackView.arrangedSubviews
+                    .map { $0 as! ThemeColorTileView }
+                    .enumerated()
+                    .forEach { horizontalCount, tileView in
+                        tileView.delegate = self
+                        let alpha = CGFloat(100 - 10 * horizontalCount)
+                        let color = TileColorType(rawValue: verticalCount)?.loadColor(alpha: alpha)
+                        tileView.backgroundColor = color
+                    }
+            }
     }
     
     @objc private func findSameColorTileView(notification: Notification) {
