421bd8565f896f89748234d3757198438623fc98
0429oonishi <oonishireon110@gmail.com>
2021-07-13 01:53:35 +0900

    cellのタップイベントを取得

--- ThemeColor-Sample/Views/Cells/AccordionColorTableViewCell.swift
+++ ThemeColor-Sample/Views/Cells/AccordionColorTableViewCell.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 final class AccordionColorTableViewCell: UITableViewCell {
-
+    
     @IBOutlet private weak var leftTileView: ThemeColorTileView!
     @IBOutlet private weak var centerTileView: ThemeColorTileView!
     @IBOutlet private weak var rightTileView: ThemeColorTileView!
+    
+    var onTapEvent: ((UIView) -> Void)?
     
     override func prepareForReuse() {
         super.prepareForReuse()
@@ -19,13 +21,31 @@ final class AccordionColorTableViewCell: UITableViewCell {
         leftTileView.layer.cornerRadius = 0
         centerTileView.layer.cornerRadius = 0
         rightTileView.layer.cornerRadius = 0
-
+        
     }
     
-    func configure(colors: [UIColor]) {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        leftTileView.delegate = self
+        centerTileView.delegate = self
+        rightTileView.delegate = self
+        
+    }
+    
+    func configure(colors: [UIColor], onTapEvent: @escaping ((UIView) -> Void)) {
+        self.onTapEvent = onTapEvent
         leftTileView.backgroundColor = colors[0]
         centerTileView.backgroundColor = colors[1]
         rightTileView.backgroundColor = colors[2]
+    }
+    
+}
+
+extension AccordionColorTableViewCell: ThemeColorTileViewDelegate {
+    
+    func tileViewDidTapped(selectedView: UIView) {
+        onTapEvent?(selectedView)
     }
     
 }
